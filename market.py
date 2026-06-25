dfbfbb6f70746203ce1c4004e335ac8a1a76fd12
nasriddinov1018-ctrl <nasriddinov1018@gmail.com>
2026-06-25 23:44:59 +0500

    Update market.py
    Just

--- market.py
+++ market.py
@@ -1,12 +1,29 @@
 import requests
+import pandas as pd
+from config import API_KEY
 
-API_KEY = "d6516ae7d2fe48969352b441 f7ba6e3d"
+BASE_URL = "https://api.twelvedata.com/time_series"
 
-def get_price(symbol="EUR/USD", interval="15min"):
-    url = (
-        f"https://api.twelvedata.com/time_series?"
-        f"symbol={symbol}&interval={interval}&outputsize=200&apikey={API_KEY}"
-    )
+def get_data(symbol="EUR/USD", interval="15min"):
 
-    response = requests.get(url)
-    return response.json()
+    params = {
+        "symbol": symbol,
+        "interval": interval,
+        "outputsize": 200,
+        "apikey": API_KEY,
+    }
+
+    r = requests.get(BASE_URL, params=params)
+    data = r.json()
+
+    if "values" not in data:
+        return None
+
+    df = pd.DataFrame(data["values"])
+
+    df = df.iloc[::-1]
+
+    for col in ["open", "high", "low", "close"]:
+        df[col] = df[col].astype(float)
+
+    return df
